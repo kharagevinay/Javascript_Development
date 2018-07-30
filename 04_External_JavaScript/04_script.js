477c576352c7df71084e74f46b2819c0e2991087
vinz <41065132+vinaykharage@users.noreply.github.com>
2018-07-30 20:26:43 +0530

    Javascript

---
+++ 04_External_JavaScript/04_script.js
@@ -0,0 +1,15 @@
+// Add your JavaScript code here
+
+function blue() {
+    document.querySelector('#green-p').style.backgroundColor='blue';
+}
+function red() {
+    document.querySelector('#green-p').style.backgroundColor='red';
+}
+function fb() {
+    document.querySelector('#myImage').setAttribute('src','../img/facebook.jpg');
+}
+function yt() {
+    document.querySelector('#myImage').setAttribute('src','../img/youtube.jpg');
+}
+
